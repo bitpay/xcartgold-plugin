1515b5a5c89cd71a628996fc4f63d5efe523bbb2
evan <elahurd3@gatech.edu>
2013-06-25 10:58:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+©2011 BIT-PAY LLC.
+Permission is hereby granted to any person obtaining a copy of this software
+and associated documentation for use and/or modification in association with
+the bitpay.com service.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Bitcoin payment module for X-Cart using the bitpay.com service.
+
+Installation
+------------
+1. Copy these files into your xcart/ directory (e.g. ~/www/xcart/ or ~/www/).  They will not overwrite any existing files.
+2. Run modules/Bitpay/install.sql on your Xcart database (e.g. "mysql -u [user] -p [xcartdb] < install.sql OR copy the contents into phpMyAdmin).
+
+Configuration
+-------------
+1. Create an API key at bitpay.com by clicking My Account > API Access Keys > Add New API Key.
+2. In your XCart admin panel, go to Settings > Payment Methods > Payment Gateways.
+3. Change Your Country to All Countries, select Bitpay and click Add.
+4. Click Payment Methods tab, check the box next to Bitpay and click Apply Changes.
+5. In the same Bitpay section click Configure. 
+6. Enter your API key from step 1.
+7. Select a transaction speed. The high speed will send a confirmation as soon as a transaction is received in the bitcoin network (usually a few seconds). A medium speed setting will typically take 10 minutes. The low speed setting usually takes around 1 hour. See the bitpay.com merchant documentation for a full description of the transaction speed settings.
+8. Choose the currency that corresponds to your store's currency from the drop-down list.
+9. Click Update.
+
+Usage
+-------------
+When a shopper chooses the Bitcoin payment method, they will be redirected to Bitpay.com where they will pay an invoice.  Bitpay will then notify your Xcart system that the order was paid for.  The customer will be presented with a button to return to your store.  
+
+The order status in the admin panel will be "Processed" if payment has been confirmed. 
+
+Note: This extension does not provide a means of automatically pulling a current BTC exchange rate for presenting BTC prices to shoppers.
+
+Change Log
+-------------
+Version 1
+  - Initial version, tested against Xcart Gold 4.5.5
+  - Tested against X-Cart Gold 4.6.0
---
+++ payment/ps_bitpay.php
@@ -0,0 +1,85 @@
+<?php
+require './auth.php';	
+require $xcart_dir.'/modules/Bitpay/bp_lib.php';
+
+$module_params = func_get_pm_params('ps_bitpay.php');
+$api_key = $module_params['param01'];
+$transaction_speed = $module_params['param02'];
+
+if (!isset($_POST['paymentid'])) { // POST from bitpay's server       
+	bpLog(file_get_contents('php://input'));
+	
+	$invoice = bpVerifyNotification($api_key);
+	if (is_string($invoice)) { 
+		bpLog($invoice); // log the error
+	}
+	else	
+	{
+		// fetch session
+		$skey = $orderids = $invoice['posData'];
+		$bill_output['sessid'] = func_query_first_cell("SELECT sessid FROM $sql_tbl[cc_pp3_data] WHERE ref='".$orderids."'");
+
+		// APC system responder
+		foreach ($_POST as $k => $v) {
+			$advinfo[] = "$k: $v";
+		}
+			
+		// update order status
+		if ($invoice['status'] == 'confirmed' or $invoice['status'] == 'complete')
+		{
+			$bill_output['sessid'] = func_query_first_cell("SELECT sessid FROM $sql_tbl[cc_pp3_data] WHERE ref='".$orderids."'");
+				
+			$bill_output['code'] = 1;			
+			$bill_output['billmsg'] = 'Order paid for';
+			bpLog($bill_output);
+			require($xcart_dir.'/payment/payment_ccend.php');
+    
+		}
+		#elseif (invoice['status'] == 'expired')
+			#$bill_output['code'] = 2;
+			#$bill_output['billmes'] = 'expired';
+			#require($xcart_dir.'/payment/payment_ccend.php');
+
+	}
+	
+} 
+else { // POST from customer placing the order
+
+    if (!defined('XCART_START')) { header("Location: ../"); die("Access denied"); }	
+	
+	// associate order id with session
+	$_orderids = join("-",$secure_oid);
+    if (!$duplicate)
+        db_query("REPLACE INTO $sql_tbl[cc_pp3_data] (ref,sessid,trstat) VALUES ('".$_orderids."','".$XCARTSESSID."','GO|".implode('|',$secure_oid)."')");
+	
+	// create invoice
+	$options = array(
+		//'currency' => $currency,
+		'currency' => $module_params['param03'],
+		'notificationURL' => $current_location.'/payment/ps_bitpay.php',
+		'redirectURL' => $current_location.'/order.php?orderid='.$_orderids,
+		'transactionSpeed' => $transaction_speed,
+		'apiKey' => $api_key,
+		'buyerName' => $bill_firstname . ' ' . $bill_lastname,
+		'buyerAddress1' => $userinfo['s_address'],
+		'buyerCity' => $userinfo['s_city'],
+		'buyerState' => $userinfo['s_statename'],
+		'buyerZip' => $userinfo['s_zipcode'],
+		'buyerCountry' => $userinfo['s_country'],
+		'buyerEmail' => $userinfo['email']
+		);
+	bpLog($options);
+	$invoice = bpCreateInvoice($_orderids, $cart['total_cost'], $_orderids, $options);
+	bpLog($invoice);
+	
+	if (isset($invoice['error']))
+	{
+		bpLog($invoice['error']);
+	}
+	else
+	{
+		// headers already sent by xcart, so use JS
+		print "<script> window.location = '$invoice[url]'; </script>"; 
+		exit;
+	}
+}
